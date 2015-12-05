fe4e40f3a83849f94bef1175981088752f7335b1
Andrew Rosa <andrewhr@me.com>
2015-12-05 12:17:29 -0200

    Draft letter selection on board
    Despite beign completely functional, this approach probably should get
    some love later on, extracting most of the styles to separate CSS rules.
    Otherwise, everything is pretty OK IMO

--- src/cljs/vaijoao/board.cljs
+++ src/cljs/vaijoao/board.cljs
@@ -1,15 +1,40 @@
 (ns vaijoao.board
-  (:require [vaijoao.game :as game]))
+  (:require [reagent.core :as r]
+            [vaijoao.game :as game]))
 
-(defonce board-state (-> (game/make-board ["d" "o" "g" "e"
-                                           "o" "a" "b" "k"
-                                           "g" "e" "m" "p"
-                                           "e" "w" "j" "l"])
-                         (game/add-player "foobar")
-                         (game/add-player "barbaz")))
+(defonce board-state (r/atom (-> (game/make-board ["d" "o" "g" "e"
+                                                   "o" "a" "b" "k"
+                                                   "g" "e" "m" "p"
+                                                   "e" "w" "j" "l"])
+                                 (game/add-player "foobar")
+                                 (game/add-player "barbaz"))))
 
-(defn board-col [col]
-  [:td col])
+(defonce board-players (r/atom {"foobar" {:color "red"}
+                                "barbaz" {:color "blue"}}))
+
+(comment
+  (swap! board-state game/select "foobar" 0 0)
+  (swap! board-state game/capture-selection "foobar"))
+
+(defn ^:private wrap-with-selection [body player]
+  [:span {:class "player-selection"
+          :style {:display "inline-block"
+                  :border-width 3
+                  :border-style "solid"
+                  :border-color (get-in @board-players [player :color])}} body])
+
+(defn letter-box [letter]
+  [:span {:style {:display     "inline-block"
+                  :width       "2em"
+                  :height      "2em"
+                  :text-align  "center"
+                  :line-height "2em"}} letter])
+
+(defn board-col [{:keys [letter players]}]
+  (letfn [(fill-letter-box-with-player-gap [players]
+            (* 3 (- (count @board-players) (count players))))]
+    [:td {:style {:padding (fill-letter-box-with-player-gap players)}}
+     (reduce wrap-with-selection [letter-box letter] players)]))
 
 (defn board-row [row]
   [:tr
@@ -23,4 +48,4 @@
       [board-row row])]])
 
 (defn board-page []
-  [:div [board board-state]])
+  [:div [board @board-state]])
