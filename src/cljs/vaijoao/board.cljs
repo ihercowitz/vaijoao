bdb74ffa679b603b6663764bff266e91d47f3d37
Andrew Rosa <andrewhr@me.com>
2015-12-05 02:05:55 -0200

    Add checks for letter availability
    Add validations to our selection logic so only adjacent letters could be
    selected after the first one. The adjency logic only cares about
    horizontal or vertical adjency, so no diagonals are allowed.

--- src/cljs/vaijoao/board.cljs
+++ src/cljs/vaijoao/board.cljs
@@ -23,9 +23,27 @@
   {:pre [(and (<= 0 row (dec rows)) (<= 0 col (dec cols)))]}
   (+ (* row cols) col))
 
+(defn ^:private neighbors-of
+  [{:keys [rows cols] :as board} row col]
+  (letfn [(within-bounds? [[r c]] (and (<= 0 r (dec rows))
+                                       (<= 0 c (dec cols))))]
+    (->> [[row (inc col)] [row (dec col)] [(inc row) col] [(dec row) col]]
+         (filter within-bounds?)
+         (map (partial apply index-of board))
+         (into #{}))))
+
+(defn available?
+  "Checks for availability of a letter to be selected. Only letters adjacents
+   with the last selected one are available. Any letter is available for an
+   empty board."
+  [{:keys [selected] :as board} row col]
+  (or (empty? selected)
+      (contains? (neighbors-of board row col) (last selected))))
+
 (defn select
   "Selects a letter by it's row and column index (zero-based)"
   [board row col]
+  {:pre [(available? board row col)]}
   (update board :selected #(conj % (index-of board row col))))
 
 (defn clear-selection
@@ -41,12 +59,18 @@
 (comment
   (-> (make-board ["a" "b" "c" "d"])
       (select 0 0)
+      (available? 0 1))                                     ;; => true
+  (-> (make-board ["a" "b" "c" "d"])
+      (select 0 0)
+      (available? 1 1))                                     ;; => false
+  (-> (make-board ["a" "b" "c" "d"])
+      (select 0 0)
       (select 0 1)
       (select 1 1)
       (current-word))
   (-> (make-board ["a" "b" "c" "d"])
       (select 0 0)
-      (select 1 1)
+      (select 0 1)
       (clear-selection)
       (current-word))
   )
